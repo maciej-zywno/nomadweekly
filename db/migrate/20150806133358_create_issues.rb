4701df3e22fa24135a252bf101917b44ed679fba
Kamil Dzierbicki <dzierbicki.kamil@outlook.com>
2015-08-06 16:21:51 +0200

    added admin panel

--- db/migrate/20150806133358_create_issues.rb
+++ db/migrate/20150806133358_create_issues.rb
@@ -3,7 +3,6 @@ class CreateIssues < ActiveRecord::Migration
     create_table :issues do |t|
       t.string :author
       t.text :description
-      t.references :link, index: true, foreign_key: true
 
       t.timestamps null: false
     end
