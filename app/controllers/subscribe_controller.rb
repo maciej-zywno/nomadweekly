f0326d9a212cc5fa1ccc6c88a7d4b408c4590bbe
Maciej Zywno <m.zywno@gmail.com>
2015-08-07 20:35:56 +0200

    futher refactor mailchimp subscription

--- app/controllers/subscribe_controller.rb
+++ app/controllers/subscribe_controller.rb
@@ -1,26 +1,9 @@
 class SubscribeController < ApplicationController
 
   def create
-    begin
-      mailchimp_subscribe(params[:email])
-
-      flash[:success] = "#{params[:email]} subscribed successfully"
-    rescue Mailchimp::ListAlreadySubscribedError
-      flash[:error] = "#{params[:email]} is already subscribed to the list"
-    rescue Mailchimp::ListDoesNotExistError
-      flash[:error] = "The list could not be found"
-    rescue Mailchimp::Error => ex
-      flash[:error] = ex.message ? ex.message : "An unknown error occurred"
-    end
-
+    success, message = MailchimpClient.new.subscribe(params[:email])
+    flash[success] = message
     redirect_to root_path
   end
-
-  private
-
-    def mailchimp_subscribe(email)
-      mc = Mailchimp::API.new(Figaro.env.mailchimp_api_key)
-      mc.lists.subscribe(Figaro.env.mailchimp_list_id, {'email' => email})
-    end
 
 end
---
+++ app/services/mailchimp_client.rb
@@ -0,0 +1,27 @@
+class MailchimpClient
+
+  MAILCHIMP_API_KEY = Figaro.env.mailchimp_api_key
+  MAILCHIMP_LIST_ID = Figaro.env.mailchimp_list_id
+
+  def subscribe(email)
+    begin
+      _subscribe(email)
+
+      [:success, "#{email} subscribed successfully"]
+    rescue Mailchimp::ListAlreadySubscribedError
+      [:error, "#{email} is already subscribed to the list"]
+    rescue Mailchimp::ListDoesNotExistError
+      [:error, "The list #{MAILCHIMP_LIST_ID} could not be found"]
+    rescue Mailchimp::Error => ex
+      [:error, ex.message ? ex.message : 'An unknown error occurred']
+    end
+  end
+
+  private
+
+    def _subscribe(email)
+      mc = Mailchimp::API.new(MAILCHIMP_API_KEY)
+      mc.lists.subscribe(MAILCHIMP_LIST_ID, {'email' => email})
+    end
+
+end
